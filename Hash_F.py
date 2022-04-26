66bd1cb21aee79b7db07faa58c733514d8c6dfb4
Cleankz <92687535+Cleankz@users.noreply.github.com>
2022-04-26 18:43:58 +0300

    Update Hash_F.py

--- Hash_F.py
+++ Hash_F.py
@@ -1,3 +1,7 @@
+import sys
+import random
+
+from attr import has
 class HashTable:
     def __init__(self, sz, stp):
         self.size = sz
@@ -5,24 +9,58 @@ class HashTable:
         self.slots = [None] * self.size
 
     def hash_fun(self, value):
-        array = self.slots
-        if array.index(value) != 0:
-            return array.index(value)
-        else:
-            return None
+         idx_of_slot = sys.getsizeof(value)
+         # в качестве value поступают строки!
 
+         # всегда возвращает корректный индекс слота
+         return idx_of_slot
     def seek_slot(self, value):
+        idx = 0
+        for i in range(len(self.slots)):
+            if self.slots[i] is not None:
+                idx = (idx + self.hash_fun(self.slots[i]))
+        idx = idx % self.size
+        if idx == 0 and self.slots[idx] is None:
+            return idx
+        a = 0
+        if idx > len(self.slots) - 1  or self.slots[idx] is not None:
+            for i in range(1,len(self.slots)):
+                a =  a + idx + (i * self.hash_fun(self.slots[i]))
+                new_id = a % len(self.slots)
+                if self.slots[new_id] is None:
+                    return new_id
+        if self.slots[idx] is None:
+            return idx
+        x = 0
+        for i in range(len(self.slots)):
+            if self.slots[i] is not None:
+                x += 1
+        if len(self.slots)  - x == 1:
+            for i in range(len(self.slots)):
+                if self.slots[i] is None:
+                    return i
+        return None
          # находит индекс пустого слота для значения, или None
-         return None
 
     def put(self, value):
          # записываем значение по хэш-функции
-         
+        slot_num = self.seek_slot(value)
+        if slot_num is not None:
+            self.slots[slot_num] = value
+        else:
+            return None
          # возвращается индекс слота или None,
          # если из-за коллизий элемент не удаётся
          # разместить 
-         return None
 
     def find(self, value):
-         # находит индекс слота со значением, или None
-         return None
+        for i in range(len(self.slots)):
+            if self.slots[i] == value:
+                return i
+        return None
+#hash = HashTable(17,3)
+#for i in range(20):
+    #print(hash.put(str(random.randint(0,17))))
+
+#for i in range(17):
+    #print(hash.find(str(random.randint(0,17))))
